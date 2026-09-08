a30c60c6852f0cd6dc714876ab800486b8588517
friedenberg <1baggzpr@starbrandshoes.com>
2026-09-08 21:03:49 +0000

    bump flake inputs: bats, conformist, nixpkgs-master (+1 more)
    bats: 6b45854 → 6b8064b
    conformist: 639a495 → 7ed393c
    nixpkgs-master: 567a49d → f13ff45
    rust-overlay: 4748ec2 → 6ae57a7

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:amarbel-llc/nixpkgs";
-    nixpkgs-master.url = "github:NixOS/nixpkgs/567a49d1913ce81ac6e9582e3553dd90a955875f";
+    nixpkgs-master.url = "github:NixOS/nixpkgs/f13ff45afd1bb73e640eaa08a7066dbed07e3238";
     utils.url = "https://flakehub.com/f/numtide/flake-utils/0.1.102";
 
     # conformist: the linter/formatter multiplexer (treefmt-nix's successor).
